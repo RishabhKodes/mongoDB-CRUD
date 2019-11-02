91a25bb7346de4544120477788063f07dbb83387
RishabhKodes <rishabhbhandari6@gmail.com>
2019-11-02 20:51:22 +0530

    first commit

---
+++ models/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// connect to mongodb
+// var url = `mongodb+srv://dbUser:password%401234@cluster0-hvdde.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('Connected to the DB :)') }
+    else { console.log('Error in DB connection : ' + err) }
+});
+
+
+
+
