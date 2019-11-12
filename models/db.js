e6eeca6394f44de04729f56aa99605d78a6d08a3
Rishabh Bhandari <41220684+RishabhKodes@users.noreply.github.com>
2019-11-12 22:16:13 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // connect to mongodb
 // var url = `mongodb+srv://dbUser:password%401234@cluster0-hvdde.mongodb.net/test?retryWrites=true&w=majority`;
+// add mongodb atlas link here--
 mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('Connected to the DB :)') }
     else { console.log('Error in DB connection : ' + err) }
