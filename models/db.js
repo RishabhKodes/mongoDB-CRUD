7bfc5f10b2d49275e4ff31f96ca77ea189dd940f
RishabhKodes <rishabhbhandari6@gmail.com>
2019-12-15 03:34:02 +0530

    added atlas link

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // connect to mongodb
 // var url = `mongodb+srv://dbUser:password%401234@cluster0-hvdde.mongodb.net/test?retryWrites=true&w=majority`;
 // add mongodb atlas link here--
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb+srv://dbUser:rishabhbh@cluster0-wd6rf.mongodb.net/test?retryWrites=true&w=majority',(err) => {
     if (!err) { console.log('Connected to the DB :)') }
     else { console.log('Error in DB connection : ' + err) }
 });
